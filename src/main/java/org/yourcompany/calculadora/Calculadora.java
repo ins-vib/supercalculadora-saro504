b91f3205c6d2e2e9c28de9a3e6c16a7e3f4e2188
Sam Roca Sanchez <saro504@vidalibarraquer.net>
2025-10-24 12:30:35 +0200

    Afegit 2 funcions

--- src/main/java/org/yourcompany/calculadora/Calculadora.java
+++ src/main/java/org/yourcompany/calculadora/Calculadora.java
@@ -7,6 +7,7 @@ package org.yourcompany.calculadora;
  * @author Posa el teu nom
  */
 
+import java.util.Random;
 
 public class Calculadora {
 
@@ -18,27 +19,38 @@ public class Calculadora {
         System.out.println("Nombre de dígits de -101010: " + nombreDigits(-101010));
         System.out.println("Nombre de dígits de 0: " + nombreDigits(0));
 
+        int resultat1 = numeroCares(50);
+        System.out.println("Nombre de cares en 50 tirades: "+resultat1);
+        
+        int resultat2 = numeroCares(100);
+        System.out.println("Nombre de cares en 100 tirades: "+resultat2);
+
+        int resultat3 = numeroCares(1000);
+        System.out.println("Nombre de cares en 1.000 tirades: "+resultat3);
+
+        double resultat4 = preuCinema(5, true, true);
+        System.out.println("Preu final: "+resultat4+"€.");
+
+        double resultat5 = preuCinema(10, false, true);
+        System.out.println("Preu final: "+resultat5+"€.");
+
         // Exemples de crida per al mètode sumaPrimersNumeros
         // System.out.println("Suma dels primers 5 números: " + sumaPrimersNumeros(5));
         // System.out.println("Suma dels primers 10 números: " + sumaPrimersNumeros(10));
         // System.out.println("Suma dels primers 15 números: " + sumaPrimersNumeros(15));
-
         // Exemples de crida per al mètode calcularFactorial
         // System.out.println("Factorial de 5: " + calcularFactorial(5));
         // System.out.println("Factorial de 7: " + calcularFactorial(7));
         // System.out.println("Factorial de 10: " + calcularFactorial(10));
-
         // Exemples de crida per al mètode sumaQuadrats
         // System.out.println("Suma dels quadrats dels primers 3 números: " + sumaQuadrats(3));
         // System.out.println("Suma dels quadrats dels primers 5 números: " + sumaQuadrats(5));
         // System.out.println("Suma dels quadrats dels primers 7 números: " + sumaQuadrats(7));
-
         // Exemples de crida per al mètode calcularPotencia
         // System.out.println("2 elevat a la potència 3: " + calcularPotencia(2, 3));
         // System.out.println("5 elevat a la potència 4: " + calcularPotencia(5, 4));
         // System.out.println("3 elevat a la potència 5: " + calcularPotencia(3, 5));
-
-       
+        
         
     }
 
@@ -60,6 +72,50 @@ public class Calculadora {
             comptador++;
         }
         return comptador;
+    }
+
+    public static int numeroCares(int repeticions) {
+
+        // Variables
+        int cara=0,creu=0,num;
+        
+        // Bucle for
+        for (int i=1;i<=repeticions;i++) {
+            Random generator = new Random();
+            num = generator.nextInt(0,2);
+            // System.out.println(num);
+            if (num == 0) {
+                cara++;
+            } else if (num == 1) {
+                creu++;
+            }
+        }
+        
+        return cara;
+
+    }
+
+    public static double preuCinema(double preuBase, boolean esCapDeSetmana, boolean teCarnetJove) {
+        
+        // Variables
+        double extra;
+        double descompte;
+        double preuFinal;
+
+        // Condicionals
+        if (esCapDeSetmana == true) {
+            extra = preuBase * 10 / 100;
+            preuFinal = preuBase + extra;
+        } else {
+            preuFinal = preuBase;
+        }
+        if (teCarnetJove == true) {
+            descompte = preuBase * 15 / 100;
+            preuFinal -= descompte;
+        }
+        
+        return preuFinal;
+
     }
 
 
